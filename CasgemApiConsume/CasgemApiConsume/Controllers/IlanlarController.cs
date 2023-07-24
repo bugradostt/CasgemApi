2361222fe3a7ffa60ee236edaec9785b767da38c
bugradostt <bugradostt@gmail.com>
2023-07-24 21:11:50 +0300

    ilan listesinin servisi yazıldı

--- CasgemApiConsume/CasgemApiConsume/Controllers/IlanlarController.cs
+++ CasgemApiConsume/CasgemApiConsume/Controllers/IlanlarController.cs
@@ -24,5 +24,18 @@ namespace CasgemApiConsume.Controllers
             }
             return View();
         }
+
+        public async Task<IActionResult> IlanDetayi()
+        {
+            //var client = _httpClientFactory.CreateClient();
+            //var responseMessage = await client.GetAsync("https://localhost:44332/api/UserIlan");
+            //if (responseMessage.IsSuccessStatusCode)
+            //{
+            //    var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            //    var values = JsonConvert.DeserializeObject<List<ResultIlanlarDto>>(jsonData);
+            //    return View(values);
+            //}
+            return View();
+        }
     }
 }
